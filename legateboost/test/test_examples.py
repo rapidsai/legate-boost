ce6136e05bff6f1d7fe8056edf054e50f0dace3a
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2023-07-27 10:33:36 +0000

    Slight refactor.

--- legateboost/test/test_examples.py
+++ legateboost/test/test_examples.py
@@ -1,12 +1,32 @@
-import os
+import importlib
 import subprocess
+import sys
 from pathlib import Path
 
-dirname = os.path.dirname(__file__)
-example_dir = os.path.join(dirname, "../../examples")
+dirname = Path(__file__).parent
+example_dir = dirname / "../../examples"
+noteboook_dir = example_dir / "../../examples/notebook"
 
 
 def test_examples():
-    for path in Path(example_dir).glob("*.py"):
-        cmd = ["legate", path]
+    sys.path.append(str(example_dir))
+    for path in example_dir.glob("*.py"):
+        importlib.import_module(path.stem)
+
+
+def test_notebooks():
+    sys.path.append(str(noteboook_dir))
+    for path in noteboook_dir.glob("*.ipynb"):
+        # use nbconvert to convert notebook to python script
+        cmd = [
+            "jupyter",
+            "nbconvert",
+            "--to",
+            "script",
+            "--RegexRemovePreprocessor.patterns='^%'",
+            str(path),
+        ]
         subprocess.check_call(cmd)
+        # import the script to run it in the existing python process
+        print("Running notebook: " + path + "\n")
+        importlib.import_module(path.stem)
