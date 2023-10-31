430bdd2c73d2887ea95c2e1fce6251fe30f0884f
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2023-10-31 08:18:44 +0000

    Experiement with parameter selection

--- legateboost/models/krr.py
+++ legateboost/models/krr.py
@@ -1,3 +1,5 @@
+from scipy.special import lambertw
+
 import cunumeric as cn
 
 from .base_model import BaseModel
@@ -7,7 +9,7 @@ def l2(X, Y):
     XX = cn.einsum("ij,ij->i", X, X)[:, cn.newaxis]
     YY = cn.einsum("ij,ij->i", Y, Y)
     XY = 2 * cn.dot(X, Y.T)
-    return XX + YY - XY
+    return cn.maximum(XX + YY - XY, 0.0)
 
 
 class KRR(BaseModel):
@@ -51,35 +53,73 @@ class KRR(BaseModel):
         Indices of the training data used to fit the model.
     """
 
-    def __init__(self, n_components=100, alpha=1e-5, sigma=None):
+    def __init__(self, n_components=100, alpha=1e-5, sigma=None, distance_init=None):
         self.num_components = n_components
         self.alpha = alpha
         self.sigma = sigma
+        self.distance_init = distance_init
 
     def _apply_kernel(self, X):
         return self.rbf_kernel(X, self.X_train)
 
     def _fit_components(self, X, g, h) -> "KRR":
         # fit with fixed set of components
-        K_mm = self._apply_kernel(self.X_train)
         K_nm = self._apply_kernel(X)
+        K_mm = self._apply_kernel(self.X_train)
         num_outputs = g.shape[1]
         self.betas_ = cn.zeros((self.X_train.shape[0], num_outputs))
 
         for k in range(num_outputs):
             W = cn.sqrt(h[:, k])
-            Kw = K_nm * W[:, cn.newaxis]
+            # Make sure we are working in 64 bit for numerical stability
+            Kw = K_nm.astype(cn.float64) * W[:, cn.newaxis]
             yw = W * (-g[:, k] / h[:, k])
             self.betas_[:, k] = cn.linalg.lstsq(
-                Kw.T.dot(Kw) + self.alpha * K_mm, cn.dot(Kw.T, yw)
+                Kw.T.dot(Kw) + self.alpha * K_mm, cn.dot(Kw.T, yw), rcond=None
             )[0]
         return self
 
+    def opt_sigma(self, D_2):
+        n = D_2.shape[1]
+        D = cn.sqrt(D_2)
+        if self.distance_init is None or self.distance_init == "lmax_mean":
+            mins = self.X_train.min(axis=0)
+            maxs = self.X_train.max(axis=0)
+            lmax = cn.mean(maxs - mins)
+            p = self.X_train.shape[1]
+            d = 2 * lmax / (((n - 1) ** (1 / p) - 1) * cn.pi)
+        elif self.distance_init == "lmax_eucl":
+            lmax = D.max()
+            p = self.X_train.shape[1]
+            d = 2 * lmax / (((n - 1) ** (1 / p) - 1) * cn.pi)
+        elif self.distance_init == "lmax":
+            mins = self.X_train.min(axis=0)
+            maxs = self.X_train.max(axis=0)
+            lmax = cn.max(maxs - mins)
+            p = self.X_train.shape[1]
+            d = 2 * lmax / (((n - 1) ** (1 / p) - 1) * cn.pi)
+        elif self.distance_init == "median":
+            bool_mask = (
+                cn.arange(D.shape[0])[:, cn.newaxis] == self.indices[cn.newaxis, :]
+            )
+            # set the self distances to infinity
+            D[bool_mask] = cn.inf
+            min_distance = D.min(axis=0)
+            d = 2 / cn.pi * cn.median(min_distance[min_distance != cn.inf])
+
+        w_arg = -self.alpha * cn.exp(0.5) / (2 * n)
+        if w_arg < -cn.exp(-1):
+            return d * cn.sqrt(3 / 2)
+        w_0 = cn.real(lambertw(w_arg, k=0))
+        sigma = d / cn.sqrt(2) * cn.sqrt(1 - 2 * w_0)
+        return sigma
+
     def rbf_kernel(self, X, Y):
-        K = l2(X, Y)
+        D_2 = l2(X, Y)
+
         if self.sigma is None:
-            self.sigma = cn.sqrt(K.mean())
-        return cn.exp(-K / (2 * self.sigma * self.sigma))
+            self.sigma = self.opt_sigma(D_2)
+        return cn.exp(-D_2 / (2 * self.sigma * self.sigma))
 
     def fit(
         self,
