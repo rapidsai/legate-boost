ae99e29f84214be9de3f34016b315e11babc56fb
Jiaming Yuan <jm.yuan@outlook.com>
2024-01-03 03:22:42 +0800

    flake8.

--- legateboost/special.py
+++ legateboost/special.py
@@ -15,7 +15,7 @@
 from enum import IntEnum
 
 import cunumeric as cn
-from legate.core import LogicalStore, get_legate_runtime, types as ty
+from legate.core import get_legate_runtime, types as ty
 
 from .library import user_context, user_lib
 from .utils import get_store
