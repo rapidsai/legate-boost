8cbf7c06ccc596d7e19f79fad0595b28b35ab805
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2024-07-09 03:38:58 -0700

    Classifier and local attributions

--- legateboost/shapley.py
+++ legateboost/shapley.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from sklearn.base import is_regressor
 from sklearn.utils.validation import check_random_state
 
 import cunumeric as cn
@@ -18,6 +19,10 @@ def global_shapley_attributions(
     n_samples: int = 5,
     assert_efficiency: bool = False,
 ):
+    def predict_fn(X):
+        fn = model.predict if is_regressor(model) else model.predict_proba
+        return fn(X)
+
     if metric is None:
         metric = model._metrics[0]
     random_state = check_random_state(random_state)
@@ -28,8 +33,9 @@ def global_shapley_attributions(
     y_temp = cn.repeat(y, X_background.shape[0], axis=0)
     w = cn.ones(y_temp.shape[0])
     X_temp = cn.zeros((X.shape[0], X_background.shape[0], X.shape[1]))
+    X_temp[:, :, :] = X_background[cn.newaxis, :, :]
     # null coalition
-    pred = model.predict(X_temp.reshape(X.shape[0] * X_background.shape[0], -1))
+    pred = predict_fn(X_temp.reshape(X.shape[0] * X_background.shape[0], -1))
     null_loss = metric.metric(y_temp, pred, w)
 
     # antithetic sampling
@@ -41,7 +47,7 @@ def global_shapley_attributions(
         previous_loss = null_loss
         for feature in p:
             X_temp[:, :, feature] = X[:, cn.newaxis, feature]
-            pred = model.predict(X_temp.reshape(X.shape[0] * X_background.shape[0], -1))
+            pred = predict_fn(X_temp.reshape(X.shape[0] * X_background.shape[0], -1))
             loss = metric.metric(y_temp, pred, w)
             v[feature, i] = loss - previous_loss
             previous_loss = loss
@@ -57,6 +63,69 @@ def global_shapley_attributions(
     std = cn.std(v, axis=1, ddof=1) / cn.sqrt(n_samples)
 
     if assert_efficiency:
-        full_coalition_loss = metric.metric(y, model.predict(X), cn.ones(y.shape[0]))
+        full_coalition_loss = metric.metric(y, predict_fn(X), cn.ones(y.shape[0]))
         assert cn.isclose(null_loss + cn.sum(shapley_values), full_coalition_loss)
     return null_loss, shapley_values, std
+
+
+def local_shapley_attributions(
+    model: BaseModel,
+    X: cn.array,
+    X_background: cn.array,
+    random_state=None,
+    n_samples: int = 5,
+    assert_efficiency: bool = False,
+):
+    def predict_fn(X):
+        fn = model.predict if is_regressor(model) else model.predict_proba
+        p = fn(X)
+        if p.ndim == 1:
+            return p.reshape(-1, 1)
+        return p
+
+    random_state = check_random_state(random_state)
+    X_temp = cn.zeros((X.shape[0], X_background.shape[0], X.shape[1]))
+    X_temp[:, :, :] = X_background[cn.newaxis, :, :]
+    # null coalition
+    null_pred = predict_fn(X_temp.reshape(X.shape[0] * X_background.shape[0], -1))
+    n_outputs = null_pred.shape[1]
+
+    # antithetic sampling
+    v_a = cn.zeros((X.shape[0], X.shape[1], n_outputs, n_samples))
+    v_b = cn.zeros((X.shape[0], X.shape[1], n_outputs, n_samples))
+
+    def eval_sample(p, v):
+        X_temp[:, :, :] = X_background[cn.newaxis, :, :]
+        previous_pred = null_pred
+        for feature in p:
+            X_temp[:, :, feature] = X[:, cn.newaxis, feature]
+            pred = predict_fn(X_temp.reshape(X.shape[0] * X_background.shape[0], -1))
+            v[:, feature, :, i] = (
+                (pred - previous_pred)
+                .reshape(X.shape[0], X_background.shape[0], n_outputs)
+                .mean(axis=1)
+            )
+            previous_pred = pred
+
+    for i in range(n_samples):
+        p_a = random_state.permutation(X.shape[1])
+        p_b = cn.flip(p_a)
+        eval_sample(p_a, v_a)
+        eval_sample(p_b, v_b)
+
+    v = (v_a + v_b) / 2
+    shapley_values = cn.mean(v, axis=-1)
+    print(shapley_values.shape)
+    std = cn.std(v, axis=-1, ddof=1) / cn.sqrt(n_samples)
+
+    null_shap = null_pred.reshape(X.shape[0], X_background.shape[0], n_outputs).mean(
+        axis=1
+    )
+    if assert_efficiency:
+        pred = predict_fn(X)
+        assert cn.allclose(null_shap + cn.sum(shapley_values, axis=1), pred)
+
+    if n_outputs == 1:
+        shapley_values = shapley_values[:, :, 0]
+        std = std[:, :, 0]
+    return null_shap, shapley_values, std
--- legateboost/test/test_shapley.py
+++ legateboost/test/test_shapley.py
@@ -1,15 +1,15 @@
 import pytest
-from sklearn.datasets import make_classification
+from sklearn.datasets import make_classification, make_regression
 
+import cunumeric as cn
 import legateboost as lb
 
 
 @pytest.mark.parametrize("random_state", range(2))
-@pytest.mark.parametrize(
-    "metric", [None, lb.metrics.MSEMetric(), lb.metrics.ExponentialMetric()]
-)
-def test_global_shapley_attributions(random_state, metric):
-    X, y = make_classification(random_state=10, n_features=10)
+@pytest.mark.parametrize("metric", [None, lb.metrics.MSEMetric()])
+@pytest.mark.parametrize("num_outputs", [1, 2])
+def test_regressor_global_shapley_attributions(random_state, metric, num_outputs):
+    X, y = make_regression(random_state=10, n_features=10, n_targets=num_outputs)
     model = lb.LBRegressor(n_estimators=5).fit(X, y)
     null_loss, shapley, std = model.global_attributions(
         X,
@@ -19,3 +19,69 @@ def test_global_shapley_attributions(random_state, metric):
         random_state=random_state,
         assert_efficiency=True,
     )
+    assert null_loss != 0
+    assert cn.isfinite(shapley).all()
+    assert cn.isfinite(std).all()
+    assert (std >= 0).all()
+
+
+@pytest.mark.parametrize("metric", [None, lb.metrics.ExponentialMetric()])
+@pytest.mark.parametrize("num_classes", [2, 3])
+def test_classifier_global_shapley_attributions(metric, num_classes):
+    X, y = make_classification(
+        random_state=10, n_features=10, n_classes=num_classes, n_clusters_per_class=1
+    )
+    model = lb.LBClassifier(n_estimators=5, random_state=9).fit(X, y)
+    null_loss, shapley, std = model.global_attributions(
+        X,
+        y,
+        metric,
+        n_background_samples=10,
+        random_state=9,
+        assert_efficiency=True,
+    )
+    assert null_loss != 0
+    assert cn.isfinite(shapley).all()
+    assert cn.isfinite(std).all()
+    assert (std >= 0).all()
+
+
+@pytest.mark.parametrize("random_state", range(2))
+@pytest.mark.parametrize("num_outputs", [1, 2])
+def test_regressor_local_shapley_attributions(random_state, num_outputs):
+    X, y = make_regression(random_state=10, n_features=10, n_targets=num_outputs)
+    model = lb.LBRegressor(n_estimators=5, random_state=random_state).fit(X, y)
+    X_background = X[:10]
+    null_pred, shapley, std = model.local_attributions(
+        X,
+        X_background,
+        random_state=random_state,
+        assert_efficiency=True,
+    )
+    if num_outputs > 1:
+        assert shapley.shape == (X.shape[0], X.shape[1], num_outputs)
+    else:
+        assert shapley.shape == (X.shape[0], X.shape[1])
+    assert cn.isfinite(shapley).all()
+    assert cn.isfinite(std).all()
+    assert (std >= 0).all()
+
+
+@pytest.mark.parametrize("random_state", range(2))
+@pytest.mark.parametrize("num_classes", [2, 3])
+def test_classifier_local_shapley_attributions(random_state, num_classes):
+    X, y = make_classification(
+        random_state=10, n_features=10, n_classes=num_classes, n_clusters_per_class=1
+    )
+    model = lb.LBClassifier(n_estimators=5, random_state=random_state).fit(X, y)
+    X_background = X[:10]
+    null_pred, shapley, std = model.local_attributions(
+        X,
+        X_background,
+        random_state=random_state,
+        assert_efficiency=True,
+    )
+    assert shapley.shape == (X.shape[0], X.shape[1], num_classes)
+    assert cn.isfinite(shapley).all()
+    assert cn.isfinite(std).all()
+    assert (std >= 0).all()
