d734b74bd9f1de15b53328d20c5c80c63f9bff3b
James Lamb <jlamb@nvidia.com>
2024-07-26 13:39:03 -0700

    add conda builds

---
+++ ci/build_python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+export CMAKE_GENERATOR=Ninja
+
+# TODO: remove this, use RAPIDS images
+export RAPIDS_CUDA_VERSION="12.2"
+
+conda mambabuild \
+    --channel conda-forge \
+    --channel legate \
+    --no-force-upload \
+    conda/recipes/legateboost
