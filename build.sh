7db718f49e2fabdead9697fa4cdf6f54d86b8e8f
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2024-07-29 04:44:20 -0700

    Revert "Merge branch 'main' of github.com:rapidsai/legateboost into memory"
    This reverts commit ee6524173f4207e2c1e30e60e7029a98504e6f33, reversing
    changes made to 1ed5acf6b6a79269bd34dd06dc867cd73b22ff9f.

--- build.sh
+++ build.sh
@@ -7,10 +7,8 @@ set -e -u -o pipefail
 # ref: https://cmake.org/cmake/help/latest/variable/CMAKE_CUDA_ARCHITECTURES.html
 declare -r CMAKE_CUDA_ARCHITECTURES="${CUDAARCHS:-native}"
 
-legate_root=$(
-    python -c 'import legate.install_info as i; from pathlib import Path; print(Path(i.libpath).parent.resolve())'
-)
+legate_root=`python -c 'import legate.install_info as i; from pathlib import Path; print(Path(i.libpath).parent.resolve())'`
 echo "Using Legate at $legate_root"
-cmake -S . -B build -Dlegate_core_ROOT="${legate_root}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_ARCHITECTURES="${CMAKE_CUDA_ARCHITECTURES}"
+cmake -S . -B build -D legate_core_ROOT=$legate_root -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_ARCHITECTURES=${CMAKE_CUDA_ARCHITECTURES}
 cmake --build build -j
 python -m pip install -e .
--- ci/build_wheel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -u -o pipefail
-
-./build.sh
-python -m build \
-    --no-isolation \
-    --wheel \
-    --outdir dist
