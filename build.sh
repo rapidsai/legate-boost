fc30aacee89e6e28ba511e936c91537544e34445
James Lamb <jlamb@nvidia.com>
2024-08-12 09:08:29 -0700

    Revert "try getting more build logs"
    This reverts commit 31da39579585a7af0841083334ca148919fedf10.

--- build.sh
+++ build.sh
@@ -33,18 +33,18 @@ PIP_INSTALL_ARGS=(
     --no-deps
 )
 
-# ensure 'native' is used if CUDAARCHS isn't set
-# (instead of the CMake default which is a specific architecture)
-# ref: https://cmake.org/cmake/help/latest/variable/CMAKE_CUDA_ARCHITECTURES.html
-declare -r CMAKE_CUDA_ARCHITECTURES="${CUDAARCHS:-native}"
+# # ensure 'native' is used if CUDAARCHS isn't set
+# # (instead of the CMake default which is a specific architecture)
+# # ref: https://cmake.org/cmake/help/latest/variable/CMAKE_CUDA_ARCHITECTURES.html
+# declare -r CMAKE_CUDA_ARCHITECTURES="${CUDAARCHS:-native}"
 
-legate_root=$(
-    python -c 'import legate.install_info as i; from pathlib import Path; print(Path(i.libpath).parent.resolve())'
-)
-echo "Using Legate at '${legate_root}'"
+# legate_root=$(
+#     python -c 'import legate.install_info as i; from pathlib import Path; print(Path(i.libpath).parent.resolve())'
+# )
+# echo "Using Legate at '${legate_root}'"
 
-cmake -S . -B build -Dlegate_core_ROOT="${legate_root}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_ARCHITECTURES="${CMAKE_CUDA_ARCHITECTURES}"
-cmake --build build -j
+# cmake -S . -B build -Dlegate_core_ROOT="${legate_root}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_ARCHITECTURES="${CMAKE_CUDA_ARCHITECTURES}"
+# cmake --build build -j
 
 if hasArg --editable; then
     PIP_INSTALL_ARGS+=("--editable")
