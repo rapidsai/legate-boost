c98bd84d89072ea2a4801fb3f37d751931c558d0
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2024-11-15 03:51:12 -0800

    Fix cffi error

--- src/legateboost.h
+++ src/legateboost.h
@@ -1,7 +1,7 @@
 #ifndef __LEGATEBOOST_C_H__
 #define __LEGATEBOOST_C_H__
 
-enum LegateBoostOpCode : uint8_t {
+enum LegateBoostOpCode {  // NOLINT(performance-enum-size)
   _OP_CODE_BASE = 0,
   BUILD_TREE    = 1,
   PREDICT       = 2,
