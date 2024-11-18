60ce56e61cf0e5cd1acfcc44b9d0fa9eaeda3450
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2024-11-18 00:59:51 -0800

    Update CI

--- ci/run_clang_tidy.sh
+++ ci/run_clang_tidy.sh
@@ -6,4 +6,35 @@
 
 set -e -E -u -o pipefail
 
+# Ensure this is running from the root of the repo
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../;
+
+# set up conda environment
+
+# source conda settings so 'conda activate' will work
+# shellcheck disable=SC1091
+. /opt/conda/etc/profile.d/conda.sh
+
+rapids-generate-version > ./VERSION
+
+rapids-print-env
+
+rapids-dependency-file-generator \
+  --output conda \
+  --file-key py_build \
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" \
+| tee /tmp/env.yaml
+
+rapids-mamba-retry env create \
+    --yes \
+    --file /tmp/env.yaml \
+    --name test-env
+
+# Temporarily allow unbound variables for conda activation.
+set +u
+conda activate test-env
+set -u
+
+rapids-print-env
+
 ./build.sh clang-tidy
