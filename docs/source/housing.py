be448326d2ff2850e78aba32f8d838474871bcc5
Nihaal Chowdary Surpani <70621514+NihaalChowdary@users.noreply.github.com>
2025-08-25 19:56:02 -0400

    Update housing.py

--- docs/source/housing.py
+++ docs/source/housing.py
@@ -1,4 +1,4 @@
-
+# [Import libraries]
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
@@ -9,17 +9,13 @@ from joblib import dump
 import numpy as np
 import pandas as pd
 
-# ---------------------------
-# Import data
-# ---------------------------
+# [Import data]
 data = fetch_california_housing()
 X_train, X_test, y_train, y_test = train_test_split(
     data.data, data.target, test_size=0.2, random_state=42
 )
 
-# ---------------------------
-# Create and fit Legate Boost regressor
-# ---------------------------
+# [Create and fit Legate Boost regressor]
 model = lb.LBRegressor(
     n_estimators=100,
     base_models=(lb.models.Tree(max_depth=5),),
@@ -32,16 +28,12 @@ start = time()
 model.fit(X_train, y_train)
 end = time()
 
-# ---------------------------
-# Prediction
-# ---------------------------
+# [Prediction]
 istart = time()
 y_pred = model.predict(X_test)
 iend = time()
 
-# ---------------------------
-# Evaluation
-# ---------------------------
+# [Evaluation]
 mse = mean_squared_error(y_test, y_pred)
 print(f"Test MSE: {mse:.4f}")
 print(f"\nThe training time for housing exp is: {(end - start)/1000:.6f} ms")
