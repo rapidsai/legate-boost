a832d9521a3ae9d113136f523a681b1282984ac2
Nihaal Chowdary Surpani <70621514+NihaalChowdary@users.noreply.github.com>
2025-08-25 19:47:22 -0400

    Update creditscore.py

--- docs/source/creditscore.py
+++ docs/source/creditscore.py
@@ -1,3 +1,4 @@
+# [import-libraries]
 import cudf
 import pandas
 import cupy as cp
@@ -14,26 +15,22 @@ from legate_dataframe.lib.core.column import LogicalColumn
 
 rt = lg.get_legate_runtime()
 
-# ----------------------------
-# Import data
-# ----------------------------
+# [import-data]
 data = fetch_openml(data_id=46929, as_frame=True)
 xd = cudf if cp.cuda.runtime.getDeviceCount() > 0 else pandas
 df = xd.DataFrame(data.data, columns=data.feature_names)
 df['Target'] = data.target
 
-# ----------------------------
-# Convert to LogicalTable
-# ----------------------------
+# [convert-to-LogicalTable]
 if cp.cuda.runtime.getDeviceCount() > 0:
     ldf = LogicalTable.from_cudf(df)
 else:
     df = pa.Table.from_pandas(df)
     ldf = LogicalTable.from_arrow(df)
+    
+# [covert-to-LogicalTable-end]
 
-# ----------------------------
-# Replace nulls
-# ----------------------------
+# [Replace nulls]
 median_salary = df["MonthlyIncome"].median()
 median_dependents = df["NumberOfDependents"].median()
 
@@ -44,9 +41,8 @@ mnd = LogicalColumn(
     replace_nulls(LogicalColumn(ldf["NumberOfDependents"]), median_dependents)
 )
 
-# ----------------------------
-# Create a new LogicalTable with updated columns
-# ----------------------------
+# [Create-new-LogicalTable-with-updated-columns]
+
 features = ldf.get_column_names()
 nldf = LogicalTable(
     [
@@ -56,18 +52,17 @@ nldf = LogicalTable(
     features
 )
 
-# ----------------------------
-# Convert to cuPyNumeric array
-# ----------------------------
-data_arr = nldf.to_array()
-print(type(data_arr))
+# [Convert-to-cupynumeric-array]
 
+data_arr = nldf.to_array()
+
+# [convert-to-cupynumeric-array-end]
+
+# [preparing-data-for-training-and-testing]
 x = data_arr[:, :-1]   # all columns except last
 y = data_arr[:, -1]
 
-# ----------------------------
-# Splitting the data into training and testing
-# ----------------------------
+# [Splitting the data into training and testing]
 num_samples = x.shape[0]
 split_ratio = 0.8
 split_index = int(num_samples * split_ratio)
@@ -77,9 +72,7 @@ y_train = y[:split_index]
 x_test = x[split_index:]
 y_test = y[split_index:]
 
-# ----------------------------
-# Training
-# ----------------------------
+# [training]
 rt.issue_execution_fence()
 start = time()
 
@@ -92,28 +85,21 @@ model = lb.LBClassifier(
 
 rt.issue_execution_fence()
 end = time()
+# [training-end]
 
-# ----------------------------
-# Prediction
-# ----------------------------
+# [Prediction]
 predictions = model.predict(x_test)
 print(type(predictions))
 
-# ----------------------------
-# Evaluation
-# ----------------------------
+# [Evaluation]
 acc = accuracy_score(y_test, predictions)
 print("Accuracy:", acc)
 print(f"\nThe training time for creditscore exp is: {(end - start)/1000:.6f} ms")
 
-# ----------------------------
-# Save model
-# ----------------------------
+# [Save model]
 dump(model, "legate_boost_model.joblib")
 
-# ----------------------------
-# Save test data
-# ----------------------------
+# [ Save test data [
 x_test_cpu = x_test.get() if hasattr(x_test, "get") else np.array(x_test)
 y_test_cpu = y_test.get() if hasattr(y_test, "get") else np.array(y_test)
 
