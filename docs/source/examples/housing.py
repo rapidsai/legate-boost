3c41b879cc4a5d113b42cfdd62807cefda6ec5be
Nihaal Chowdary Surpani <70621514+NihaalChowdary@users.noreply.github.com>
2025-08-29 15:11:21 -0400

    Update housing.py

--- docs/source/examples/housing.py
+++ docs/source/examples/housing.py
@@ -2,7 +2,6 @@
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import cupynumeric as cn
 import legateboost as lb
 from legate.timing import time
 
